455944bdf7bd9655f4868377aa355f94dc26a3e8
Anjum <anjumunnisa1999@gmail.com>
2025-08-05 21:48:21 +0530

    hover

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+<div>
+    <App />
+  </div>
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
